71754c12c506d194968fbf74d0ee5729a4be062c
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2013-11-06 02:50:46 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,159 @@
+/* jshint laxcomma: true */
+
+(function() {
+  'use strict';
+
+  var fcount = 0;
+
+
+  function cors3() {
+    // :
+  }
+
+
+  /*
+   * inherit EventEmitter
+   */
+
+  cors3.prototype = Object.create(EventEmitter.prototype);
+
+
+  /*
+   * defaults
+   */
+
+  cors3.prototype.useAcl          = 'private';
+  cors3.prototype.useSignatureUrl = '/sign/my/s3';
+
+
+  /*
+   * @api public
+   */
+
+  cors3.prototype.upload = function(filesObj) {
+    for (var i=0, l=filesObj.length; i<l; i+=1) {
+      var file = {_id: fcount, file: filesObj[i]};
+
+      this.emit('fileadded', file);
+      this._upload(file);
+
+      fcount+= 1; // count, used as unique id for reference
+    }
+  };
+
+
+  /*
+   * @api private
+   */
+
+  cors3.prototype._upload = function(file) {
+    var self = this;
+
+    this.signFile(file, function(err, signedFile) {
+      self.emit('filesigned', err, signedFile);
+      self.puts3(signedFile);
+    });
+  };
+
+
+  /*
+   * @api private
+   */
+
+  cors3.prototype.signFile = function(file, callback) {
+    var self = this
+      , xhr  = new XMLHttpRequest()
+      ;
+
+    var params = '?mimetype='+file.file.type+'&filename='+file.file.name;
+
+    xhr.open('GET', this.useSignatureUrl+params);
+
+    xhr.onreadystatechange = function(evt) {
+      if (this.readyState === 4 && this.status === 200) {
+        var json = JSON.parse(this.responseText);
+
+        for (var k in json) {
+          if (json.hasOwnProperty(k)) file[k] = json[k];
+        }
+
+        callback(null, file);
+
+      } else if (this.readyState === 4 && this.status !== 200) {
+        callback({status: this.status, response: this.responseText}, file);
+      }
+    };
+
+    xhr.send();
+  };
+
+
+  /*
+   * @api private
+   */
+
+  cors3.prototype.puts3 = function(file) {
+    var self = this;
+
+    this.corsit('PUT', file.signedPutUrl, function(err, xhr) {
+      if (err) {
+        self.emit('error', err, file);
+      } else {
+        xhr.onloadstart = function() {
+          self.emit('uploadstarted', file);
+        };
+
+        xhr.onload = function() {
+          if (xhr.status === 200) {
+            self.emit('uploadcompleted', file);
+          } else {
+            self.emit('uploadfailed', {status: this.status, response: this.responseText}, file);
+          }
+        };
+
+        xhr.onerror = function() {
+          self.emit('uploadfailed', 'XHR error.');
+        };
+
+        xhr.onprogress = function(evt) {
+          console.log('onprogress...');
+          if (evt.lengthComputable) self.emit('uploadprogress', file, evt.total, evt.loaded);
+        };
+
+        xhr.abort = function() {
+          self.emit('uploadaborted', file);
+        };
+      }
+
+      xhr.setRequestHeader('Content-Type', file.file.type);
+      xhr.setRequestHeader('x-amz-acl',    self.useAcl);
+
+      xhr.send(file.file);
+    });
+  };
+
+
+  /*
+   * @api private
+   */
+
+  cors3.prototype.corsit = function(method, url, callback) {
+    var xhr = new XMLHttpRequest();
+
+    if (xhr.withCredentials !== null) {
+      xhr.open('PUT', url, true);
+    } else if ('undefined' !== typeof XDomainRequest) {
+      xhr = new XDomainRequest();
+      xhr.open('PUT', url);
+    } else {
+      callback('No CORS support', null);
+      return;
+    }
+
+    callback(null, xhr);
+  };
+
+
+  this.Cors3 = cors3;
+
+}).call(this);
